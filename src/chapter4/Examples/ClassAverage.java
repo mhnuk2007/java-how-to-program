ab95da7a2aa524274fc2d4839ab4660b9f679f3a
Mohan Lal <mhnuk2007@gmail.com>
2025-04-12 00:36:56 +0500

    Implementing sentinel-Controlled Repetition

--- src/chapter4/Examples/ClassAverage.java
+++ src/chapter4/Examples/ClassAverage.java
@@ -1,7 +1,8 @@
-//Implementing Counter-Controlled Repetition
+//Implementing sentinel-Controlled Repetition
 package chapter4.Examples;
-// Fig. 4.8: ClassAverage.java
-// Solving the class-average problem using counter-controlled repetition.
+// Fig. 4.10: ClassAverage.java
+// Solving the class-average problem using sentinel-controlled repetition.
+
 
 import java.util.Scanner;
 
@@ -12,21 +13,32 @@ public class ClassAverage {
 
         // initialization phase
         int total = 0;
-        int gradeCounter = 1;
+        int gradeCounter = 0;
 
-        // processing phase uses counter-controlled repetition
-        while (gradeCounter <= 10) {
-            System.out.print("Enter grade: ");
-            int grade = input.nextInt();
+        //processing phase
+        System.out.print("Enter grade or -1 to quit: ");
+        int grade = input.nextInt();
+
+        // loop under sentinel value read from user
+        while (grade != -1) {
             total = total + grade;
             gradeCounter = gradeCounter + 1;
+
+            // prompt for input
+            System.out.print("Enter grade or -1 to quit: ");
+            grade = input.nextInt();
         }
 
         // termination phase
-        int average = total / 10;
+        if (gradeCounter != 0) {
+            double average = (double) total / gradeCounter;
 
-        // display total and average of grades
-        System.out.printf("%nTotal of 10 grades is %d%n", total);
-        System.out.printf("Class average is %d%n", average);
+            // display total and average of grades
+            System.out.printf("%nTotal of %d grades is %d%n", gradeCounter, total);
+            System.out.printf("Class average is %.2f%n", average);
+        } else {
+            System.out.println("No grades were entered");
+        }
+        input.close();
     }
 }
