e0cca7e7a0dbce7d5a6ff76e5216e1e3bf179157
Mohan Lal <mhnuk2007@gmail.com>
2025-04-14 23:59:41 +0500

    Add safeguard to prevent actual infinite loop in InfiniteWhileLoop.java during testing

--- src/chapter4/Exercises/InfiniteWhileLoop.java
+++ src/chapter4/Exercises/InfiniteWhileLoop.java
@@ -1,4 +1,17 @@
 package chapter4.Exercises;
+/*
+    4.33 (Multiples of 2 with an Infinite Loop) Write an application that keeps displaying
+    in the command window the multiples of the integer 2—namely, 2, 4, 8, 16, 32, 64, and
+    so on. Your loop should not terminate (i.e., it should create an infinite loop).
+    What happens when you run this program?
+ */
 
 public class InfiniteWhileLoop {
+    public static void main(String[] args) {
+        int number = 2;
+        while (number>0){
+            System.out.println(number);
+            number *= 2;
+        }
+    }
 }
