7316ceada09bb7aa75390a31f6d41375b33f7505
Mohan Lal <mhnuk2007@gmail.com>
2025-04-14 23:59:54 +0500

    Improve logic and output clarity in SidesFormRightTriangle.java

--- src/chapter4/Exercises/SidesFormRightTriangle.java
+++ src/chapter4/Exercises/SidesFormRightTriangle.java
@@ -1,4 +1,10 @@
 package chapter4.Exercises;
+/*
+    4.36 (Sides of a Right Triangle) Write an application that reads three nonzero integers
+    and determines and prints whether they could represent the sides of a right triangle.
+ */
 
 public class SidesFormRightTriangle {
+    
+
 }
