c231fcf0585f1d64d739f82df1cf10dfe9638a9f
Mohan Lal <mhnuk2007@gmail.com>
2025-04-21 12:20:13 +0500

    Calculate and display factorials from 1 to 20

--- src/chapter5/exercises/Factorials.java
+++ src/chapter5/exercises/Factorials.java
@@ -1,4 +1,20 @@
 package chapter5.exercises;
+/*
+    5.13 (Factorials) Factorials are used frequently in probability problems. The factorial of a
+    positive integer n (written n! and pronounced “n factorial”) is equal to the product
+    of the positive integers from 1 to n. Write an application that calculates the factorials
+    of 1 through 20. Use type long. Display the results in tabular format. What difficulty
+    might prevent you from calculating the factorial of 100?
+ */
 
 public class Factorials {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 20; i++) {
+            long factorial = 1;
+            for (int j = 1; j <= i; j++) {
+                factorial *= j;
+            }
+            System.out.printf("%d! = %d%n", i, factorial);
+        }
+    }
 }
