8962695ce5a7f4fffa8f143d1553163daa0cba15
Mohan Lal <mhnuk2007@gmail.com>
2025-04-21 12:20:13 +0500

    Add product calculation of odd integers from 1 to 15

--- src/chapter5/exercises/ProducOfOddIntegers.java
+++ src/chapter5/exercises/ProducOfOddIntegers.java
@@ -1,4 +1,19 @@
 package chapter5.exercises;
+/*
+    5.12 (Calculating the Product of Odd Integers) Write an application that calculates
+    the product of the odd integers from 1 to 15.
+ */
 
 public class ProducOfOddIntegers {
+    public static void main(String[] args) {
+        int product = 1;
+
+        for (int i = 1; i <= 15; i += 2) {
+            product *= i;
+        }
+
+        System.out.println("The product of odd integers from 1 to 15 is: " + product);
+    }
+
+
 }
