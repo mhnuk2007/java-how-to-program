40411571665f3099c12584a78ee4b41ea1050ec0
Mohan Lal <mhnuk2007@gmail.com>
2025-04-23 00:24:40 +0500

    Exercise 5.27: Printed 5 blocks of stars using nested loops

---
+++ src/chapter5/exercises/Ex_5_27.java
@@ -0,0 +1,17 @@
+package chapter5.exercises;
+
+public class Ex_5_27 {
+    public static void main(String[] args) {
+        int i, j, k;
+        for (i = 1; i <= 5; i++)
+        {
+            for (j = 1; j <= 3; j++)
+            {
+                for (k = 1; k <= 4; k++)
+                    System.out.print('*');
+                System.out.println();
+            } // end inner for
+            System.out.println();
+        } // end outer for
+    }
+}
