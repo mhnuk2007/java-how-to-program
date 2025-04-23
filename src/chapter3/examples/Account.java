6befe7cad570dcf5c96bc0f9ad25f72840725642
Mohan Lal <mhnuk2007@gmail.com>
2025-04-24 00:21:22 +0500

    Modify Account class and  a constructor that initializes the name.

--- src/chapter3/examples/Account.java
+++ src/chapter3/examples/Account.java
@@ -1,10 +1,13 @@
 package chapter3.examples;
-// Fig. 3.1: Account.java
-// Account class that contains a name instance variable
-// and methods to set and get its value.
+// Fig. 3.5: Account.java
+// Account class with a constructor that initializes the name.
 
 public class Account {
     private String name;
+
+    public Account(String name){
+        this.name=name;
+    }
 
     public String getName() {
         return name;
