e247a51d438d6351eda247f3a83514727cd8bfdf
Mohan Lal <mhnuk2007@gmail.com>
2025-04-24 00:40:29 +0500

    Account class with a double instance variable balance and a constructor and deposit method that perform validation

--- src/chapter3/examples/Account.java
+++ src/chapter3/examples/Account.java
@@ -1,12 +1,27 @@
 package chapter3.examples;
-// Fig. 3.5: Account.java
-// Account class with a constructor that initializes the name.
+// Fig. 3.8: Account.java
+// Account class with a double instance variable balance and a constructor
+// and deposit method that perform validation.
 
 public class Account {
     private String name;
+    private double balance;
 
-    public Account(String name){
-        this.name=name;
+    public Account(String name, double balance) {
+        this.name = name;
+        if (balance > 0.0) {
+            this.balance = balance;
+        }
+    }
+
+    public void deposit(double depositAmount){
+        if (depositAmount > 0.0) {
+            balance = balance + depositAmount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
     }
 
     public String getName() {
