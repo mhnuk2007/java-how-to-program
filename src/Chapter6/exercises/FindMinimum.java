798c97c46aa9bb61381c9d86745985f7f7861827
Mohan Lal <mhnuk2007@gmail.com>
2025-04-27 13:05:13 +0500

    Added 6.23 FindMinimum exercise - returns smallest of three floating-point numbers

---
+++ src/Chapter6/exercises/FindMinimum.java
@@ -0,0 +1,33 @@
+package Chapter6.exercises;
+/*
+    6.23 (Find the Minimum) Write a method minimum3 that returns the smallest of three
+    floatingpoint numbers. Use the Math.min method to implement minimum3. Incorporate the
+    method into an application that reads three values from the user, determines the
+    smallest value and displays the result.
+ */
+
+import java.util.Scanner;
+
+public class FindMinimum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first floating-point number: ");
+        double num1 = input.nextDouble();
+
+        System.out.print("Enter the second floating-point number: ");
+        double num2 = input.nextDouble();
+
+        System.out.print("Enter the third floating-point number: ");
+        double num3 = input.nextDouble();
+
+        System.out.printf("The smallest number is: %.2f%n", minimum3(num1, num2, num3));
+
+        input.close();
+    }
+
+    public static double minimum3(double num1, double num2, double num3) {
+        return Math.min(num1, Math.min(num2, num3));
+    }
+
+}
