e2dde1c460433d087e0ba5c418a253dd80704d75
Mohan Lal <mhnuk2007@gmail.com>
2025-04-28 17:45:00 +0500

    Update NumberSystemTable.java to use StringBuilder for manual conversions

--- src/Chapter6/exercises/NumberSystemTable.java
+++ src/Chapter6/exercises/NumberSystemTable.java
@@ -1,4 +1,5 @@
 package Chapter6.exercises;
+
 /*
      6.34 (Table of Binary, Octal and Hexadecimal Numbers) Write an application that
      displays a table of the binary, octal and hexadecimal equivalents of the decimal
@@ -11,59 +12,68 @@ public class NumberSystemTable {
         // Print table header
         System.out.printf("%-10s%-15s%-15s%-15s%n", "Decimal", "Binary", "Octal", "Hexadecimal");
         System.out.println("-------------------------------------------------------------");
+
         // Loop through decimal numbers from 1 to 256
         for (int decimal = 1; decimal <= 256; decimal++) {
-            System.out.printf("%-10d%-15s%-15s%-15s%n", decimal, decimalToBinary(decimal), decimalToOctal(decimal), decimalToHexadecimal(decimal));
+            System.out.printf("%-10d%-15s%-15s%-15s%n",
+                    decimal,
+                    decimalToBinary(decimal),
+                    decimalToOctal(decimal),
+                    decimalToHexadecimal(decimal)
+            );
         }
-
-
     }
 
-    // Manual method to convert decimal to binary
+    // Manual method to convert decimal to binary using StringBuilder
     public static String decimalToBinary(int decimal) {
         if (decimal == 0) {
             return "0";
         }
-        String binary = "";
+
+        StringBuilder binary = new StringBuilder();
+
         while (decimal > 0) {
-            binary = (decimal % 2) + binary;
+            binary.insert(0, decimal % 2);
             decimal /= 2;
         }
-        return binary;
+
+        return binary.toString();
     }
 
-    // Manual method to convert decimal to octal
+    // Manual method to convert decimal to octal using StringBuilder
     public static String decimalToOctal(int decimal) {
         if (decimal == 0) {
             return "0";
         }
-        String octal = "";
+
+        StringBuilder octal = new StringBuilder();
 
         while (decimal > 0) {
-            octal = (decimal % 8) + octal;
+            octal.insert(0, decimal % 8);
             decimal /= 8;
         }
-        return octal;
+
+        return octal.toString();
     }
 
-
-    // Manual method to convert decimal to Hexadecimal
+    // Manual method to convert decimal to hexadecimal using StringBuilder
     public static String decimalToHexadecimal(int decimal) {
         if (decimal == 0) {
             return "0";
         }
-        String hexadecimal = "";
+
+        StringBuilder hexadecimal = new StringBuilder();
+
         while (decimal > 0) {
             int remainder = decimal % 16;
             if (remainder < 10) {
-                hexadecimal = remainder + hexadecimal;
+                hexadecimal.insert(0, remainder);
             } else {
-                hexadecimal = (char) (remainder - 10 + 'A') + hexadecimal;
+                hexadecimal.insert(0, (char)(remainder - 10 + 'A'));
             }
             decimal /= 16;
-
         }
-        return hexadecimal;
+
+        return hexadecimal.toString();
     }
 }
-
