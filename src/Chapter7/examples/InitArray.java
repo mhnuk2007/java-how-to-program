47f95550b9e13836cb922003147f07dd7daa8a6f
Mohan Lal <mhnuk2007@gmail.com>
2025-04-29 13:22:10 +0500

    Update InitArray.java: Calculate array values dynamically using formula

--- src/Chapter7/examples/InitArray.java
+++ src/Chapter7/examples/InitArray.java
@@ -1,16 +1,24 @@
 package Chapter7.examples;
 
-// Fig. 7.3: InitArray.java
-// Initializing the elements of an array with an array initializer.
+// Fig. 7.4: InitArray.java
+// Calculating the values to be placed into the elements of an array.
 
 public class InitArray {
     public static void main(String[] args) {
-        // initializer list specifies the initial value for each element
-        int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+        final int ARRAY_LENGTH = 10; // constant to define array size
 
-        System.out.printf("%s%8s%n", "Index", "Value"); // column headings
+        // declare and create an array of 10 integers
+        int[] array = new int[ARRAY_LENGTH];
 
-        // output each array element's value
+        // assign values to array: each element is 2 + 2 * its index
+        for (int counter = 0; counter < array.length; counter++) {
+            array[counter] = 2 + 2 * counter;
+        }
+
+        // display column headings
+        System.out.printf("%s%8s%n", "Index", "Value");
+
+        // display index and corresponding value of each array element
         for (int counter = 0; counter < array.length; counter++) {
             System.out.printf("%5d%8d%n", counter, array[counter]);
         }
