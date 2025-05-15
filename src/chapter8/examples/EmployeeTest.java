097409a5126ad6d247acb9de54d97ccbdf14752d
Mohan Lal <mhnuk2007@gmail.com>
2025-05-15 21:05:00 +0500

    Update Employee and EmployeeTest to demonstrate static variables and methods

--- src/chapter8/examples/EmployeeTest.java
+++ src/chapter8/examples/EmployeeTest.java
@@ -1,15 +1,25 @@
 package chapter8.examples;
-// Fig. 8.9: EmployeeTest.java
-// Composition demonstration.
+// Fig. 8.13: EmployeeTest.java
+// static member demonstration.
 
 public class EmployeeTest {
     public static void main(String[] args) {
-        Date birth = new Date(7, 24, 1949);
-        Date hire = new Date(3, 12, 1988);
-        Employee employee = new Employee("Bob", "Blue", birth, hire);
+        // show that count is 0 before creating Employees
+        System.out.printf("Employees before instantiation: %d%n",
+                Employee.getCount());
 
-        System.out.println(employee);
+        // create two Employees; count should be 2
+        Employee e1 = new Employee("Susan", "Baker");
+        Employee e2 = new Employee("Bob", "Blue");
+
+        // show that count is 2 after creating two Employees
+        System.out.printf("%nEmployees after instantiation: %d%n", Employee.getCount());
+        System.out.printf("via e1.getCount(): %d%n", e1.getCount());
+        System.out.printf("via e2.getCount(): %d%n", e2.getCount());
+        System.out.printf("via Employee.getCount(): %d%n", Employee.getCount());
+
+        // get names of Employees
+        System.out.printf("%nEmployee 1: %s %s%nEmployee 2: %s %s%n",
+                e1.getFirstName(), e1.getLastName(), e2.getFirstName(), e2.getLastName());
     }
-
-
 }
