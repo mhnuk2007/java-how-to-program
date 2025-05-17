f38ebda2a839f566afb8aa9f1e34a32d16f9407a
Mohan Lal <mhnuk2007@gmail.com>
2025-05-18 00:14:19 +0500

    Update BasePlusCommissionEmployee.java with initial inheritance implementation

--- src/chapter9/examples/BasePlusCommissionEmployee.java
+++ src/chapter9/examples/BasePlusCommissionEmployee.java
@@ -1,83 +1,25 @@
 package chapter9.examples;
-// Fig. 9.6: BasePlusCommissionEmployee.java
-// BasePlusCommissionEmployee class represents an employee who receives
-// a base salary in addition to commission.
+// Fig. 9.8: BasePlusCommissionEmployee.java
+// private superclass members cannot be accessed in a subclass.
 
-public class BasePlusCommissionEmployee {
-    private final String firstName;
-    private final String lastName;
-    private final String socialSecurityNumber;
-    private double grossSales; // gross weekly sales
-    private double commissionRate;  // commission percentage
+
+public class BasePlusCommissionEmployee extends CommissionEmployee {
+
     private double baseSalary; // base salary per week
 
     // six-argument constructor
     public BasePlusCommissionEmployee(String firstName, String lastName,
                                       String socialSecurityNumber, double grossSales,
                                       double commissionRate, double baseSalary) {
-        // implicit call to Object's default constructor occurs here
+        // explicit call to superclass CommissionEmployee constructor
+        super(firstName, lastName, socialSecurityNumber,
+                grossSales, commissionRate);
+        if (baseSalary < 0.0)
+            throw new IllegalArgumentException(
+                    "Base salary must be >= 0.0");
 
-        // if grossSales is invalid throw exception
-        if (grossSales < 0.0) {
-            throw new IllegalArgumentException("Gross sales must be >= 0.0");
-        }
-
-        // if commissionRate is invalid throw exception
-        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
-            throw new IllegalArgumentException("Commission Rate must be > 0.0 and < 1.0");
-        }
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.socialSecurityNumber = socialSecurityNumber;
-        this.grossSales = grossSales;
-        this.commissionRate = commissionRate;
         this.baseSalary = baseSalary;
     } // end constructor
-
-    // return first name
-    public String getFirstName() {
-        return firstName;
-    }
-
-    // return last name
-    public String getLastName() {
-        return lastName;
-    }
-
-    // return social security number
-    public String getSocialSecurityNumber() {
-        return socialSecurityNumber;
-    }
-
-    // set gross sales amount
-    public void setGrossSales(double grossSales) {
-        // if grossSales is invalid throw exception
-        if (grossSales < 0.0) {
-            throw new IllegalArgumentException("Gross sales must be >= 0.0");
-        }
-
-        this.grossSales = grossSales;
-    }
-
-    // return gross sales amount
-    public double getGrossSales() {
-        return grossSales;
-    }
-
-    // set commission rate
-    public void setCommissionRate(double commissionRate) {
-        // if commissionRate is invalid throw exception
-        if (commissionRate <= 0.0 || commissionRate >= 1.0) {
-            throw new IllegalArgumentException("Commission Rate must be > 0.0 and < 1.0");
-        }
-        this.commissionRate = commissionRate;
-    }
-
-    // return commission rate
-    public double getCommissionRate() {
-        return commissionRate;
-    }
 
     // set base salary
     public void setBaseSalary(double baseSalary) {
@@ -94,13 +36,17 @@ public class BasePlusCommissionEmployee {
     }
 
     // calculate earnings
+    @Override
+
     public double earnings() {
+        // not allowed: commissionRate and grossSales private in superclass
         return baseSalary + (commissionRate * grossSales);
     }
 
     // return String representation of BasePlusCommissionEmployee
     @Override
     public String toString() {
+        // not allowed: attempts to access private superclass members
         return String.format("%s: %s %s%n%s: %s%n%s: %.2f%n%s: %.2f%n%s: %.2f",
                 "base-salaried commission employee", firstName, lastName,
                 "social security number", socialSecurityNumber,
