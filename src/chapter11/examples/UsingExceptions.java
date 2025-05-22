5fb1651a14dcf01145e25d42f26deadc6b622ed9
Mohan Lal <mhnuk2007@gmail.com>
2025-05-22 20:54:49 +0500

    Updated UsingExceptions.java to demonstrate stack unwinding and trace info

--- src/chapter11/examples/UsingExceptions.java
+++ src/chapter11/examples/UsingExceptions.java
@@ -1,46 +1,41 @@
 package chapter11.examples;
-// Fig. 11.5: UsingExceptions.java
-// try...catch...finally exception handling mechanism.
+// Fig. 11.6: UsingExceptions.java
+// Stack unwinding and obtaining data from an exception object.
 
 public class UsingExceptions {
     public static void main(String[] args) {
         try {
-            throwException();
-        } catch (Exception exception) { // exception thrown by throwException
-            System.err.println("Exception handled in main");
-        }
-
-        doesNotThrowException();
-    }
-
-    // demonstrate try...catch...finally
-    public static void throwException() throws Exception {
-        try { // throw an exception and immediately catch it
-            System.out.println("Method throwException");
-            throw new Exception("throwException");
-        } catch (Exception exception) {
-            System.err.println("Exception handled in throwException");
-            throw exception; // rethrow for further processing
-
-            // code here would not be reached; would cause compilation errors
-        } finally // executes regardless of what occurs in try...catch
+            method1();
+        } catch (Exception exception) // catch exception thrown in method1
         {
-            System.err.println("Finally executed in throwException");
-        }
-        // code here would not be reached; would cause compilation errors
+            System.err.printf("%s%n%n", exception.getMessage());
+            exception.printStackTrace();
 
+            // Obtain the stack-trace elements
+            StackTraceElement[] traceElements = exception.getStackTrace();
+
+            System.out.printf("%nStack trace from getStackTrace:%n");
+            System.out.println("Class\t\tFile\t\t\tLine\tMethod");
+
+            // Loop through traceElements to get structured info
+            for (StackTraceElement element : traceElements) {
+                System.out.printf("%s\t", element.getClassName());
+                System.out.printf("%s\t", element.getFileName());
+                System.out.printf("%d\t", element.getLineNumber());
+                System.out.printf("%s%n", element.getMethodName());
+            }
+        }
     }
 
-    // demonstrate finally when no exception occurs
-    public static void doesNotThrowException() {
-        try { // try block does not throw an exception
-            System.out.println("Method doesNotThrowException");
-        } catch (Exception exception) {
-            System.err.println(exception);
-        } finally {
-            System.err.println("Finally executed in doesNotThrowException");
-
-        }
-        System.out.println("End of method doesNotThrowException");
-        }
+    public static void method1() throws Exception {
+        method2();
     }
+
+    public static void method2() throws Exception {
+        method3();
+    }
+
+    public static void method3() throws Exception {
+        throw new Exception("Exception thrown in method3");
+    }
+}
