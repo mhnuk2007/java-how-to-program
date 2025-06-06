6d3c7e76fee0624e26480b97636948cf86b1e9c6
Mohan Lal <mhnuk2007@gmail.com>
2025-06-06 13:30:02 +0500

    Add DiscountCalculator: calculates discounted price using BiFunction lambda

--- src/chapter17/examples/DiscountCalculator.java
+++ src/chapter17/examples/DiscountCalculator.java
@@ -1,4 +1,24 @@
 package chapter17.examples;
+//Discount Calculator using BiFunction
+
+import java.util.Scanner;
+import java.util.function.BiFunction;
 
 public class DiscountCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the original price: ");
+        double originalPrice = input.nextDouble();
+
+        System.out.print("Enter the discount percentage: ");
+        double discountPercentage = input.nextDouble();
+
+        BiFunction<Double, Double, Double> applyDiscount = (p,d) -> p - (p*d/100);
+
+        System.out.println("Final Price: " + applyDiscount.apply(originalPrice, discountPercentage));
+
+
+    }
+
 }
