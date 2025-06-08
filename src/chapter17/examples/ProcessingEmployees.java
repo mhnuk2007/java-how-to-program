e2c3c2810d5da7989cbf7c23b2bf58ac36aa0736
Mohan Lal <mhnuk2007@gmail.com>
2025-06-08 19:13:49 +0500

    Update ProcessingEmployees with stream filtering and sorting logic

--- src/chapter17/examples/ProcessingEmployees.java
+++ src/chapter17/examples/ProcessingEmployees.java
@@ -3,13 +3,15 @@ package chapter17.examples;
 // Processing streams of Employee objects.
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class ProcessingEmployees {
     public static void main(String[] args) {
         // initialize array of Employees
         Employee[] employees = {
-                new Employee("Jason", "Red", 75000, "IT"),
+                new Employee("Jason", "Red", 5000, "IT"),
                 new Employee("Ashley", "Green", 7600, "IT"),
                 new Employee("Matthew", "Indigo", 3587.5, "Sales"),
                 new Employee("James", "Indigo", 4700.77, "Marketing"),
@@ -24,8 +26,22 @@ public class ProcessingEmployees {
         System.out.println("Complete Employee list:");
         list.stream().forEach(System.out::println);
 
+        // Predicate that returns true for salaries in the range $4000-$6000
+        Predicate<Employee> fourToSixThousand =
+                e-> e.getSalary()>=4000 && e.getSalary()<=6000;
+        // Display Employees with salaries in the range $4000-$6000
+        // sorted into ascending order by salary
+        System.out.printf("%nEmployees earning $4000-$6000 per month sorted by salary:%n");
+        list.stream()
+                .filter(fourToSixThousand)
+                .sorted(Comparator.comparing(Employee::getSalary))
+                .forEach(System.out::println);
 
-
-
+        // Display first Employee with salary in the range $4000-$6000
+        System.out.printf("%nFirst employee who earns $4000-$6000:%n%s%n",
+                list.stream()
+                        .filter(fourToSixThousand)
+                        .findFirst()
+                        .get());
     }
 }
