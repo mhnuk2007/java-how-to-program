a3ac77ba7d5325798a2fa68dc655aca158bf24d5
Mohan Lal <mhnuk2007@gmail.com>
2025-06-11 05:05:32 +0500

    Add binary search implementation in chapter19.examples package

--- src/chapter19/examples/BinarySearch.java
+++ src/chapter19/examples/BinarySearch.java
@@ -1,4 +1,35 @@
 package chapter19.examples;
 
 public class BinarySearch {
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length-1;
+
+
+        while (left<=right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] data = {2, 5, 10, 30, 34, 51, 52, 56, 77, 93};
+        int key = 25;
+
+        int result = binarySearch(data, key);
+        if (result!=-1)
+        System.out.println("Element found at index: " + result);
+        else
+            System.out.println("Element not found");
+    }
+
 }
