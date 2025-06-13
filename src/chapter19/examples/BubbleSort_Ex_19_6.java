82f901386e8ee53bb4b456d5b45749b34993c2bd
Mohan Lal <mhnuk2007@gmail.com>
2025-06-14 00:41:03 +0500

    Update BubbleSort_Ex_19_6.java

--- src/chapter19/examples/BubbleSort_Ex_19_6.java
+++ src/chapter19/examples/BubbleSort_Ex_19_6.java
@@ -1,4 +1,17 @@
 package chapter19.examples;
+/*
+Exercise 19.5: (Bubble Sort) Implement bubble sort—another simple yet inefficient sorting technique. It’s
+called bubble sort or sinking sort because smaller values gradually “bubble” their way to the top of
+the array (i.e., toward the first element) like air bubbles rising in water, while the larger values sink
+to the bottom (end) of the array. The technique uses nested loops to make several passes through
+the array. Each pass compares successive pairs of elements. If a pair is in increasing order (or the
+values are equal), the bubble sort leaves the values as they are. If a pair is in decreasing order, the
+bubble sort swaps their values in the array. The first pass compares the first two elements of the array
+and swaps their values if necessary. It then compares the second and third elements in the array. The
+end of this pass compares the last two elements in the array and swaps them if necessary. After one
+pass, the largest element will be in the last index. After two passes, the largest two elements will be
+in the last two indices. Explain why bubble sort is an O(n2) algorithm.
+ */
 
 public class BubbleSort_Ex_19_6 {
     // Bubble sort method
