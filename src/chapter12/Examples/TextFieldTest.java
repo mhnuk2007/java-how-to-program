9c73797ac9a690acddcae40ae0abe333e757f96d
Mohan Lal <mhnuk2007@gmail.com>
2025-06-18 01:21:35 +0500

    Add and update TextFieldFrame and TextFieldTest with event handling

---
+++ src/chapter12/Examples/TextFieldTest.java
@@ -0,0 +1,15 @@
+package chapter12.Examples;
+
+// Fig. 12.10: TextFieldTest.java
+// Testing TextFieldFrame.
+
+import javax.swing.JFrame;
+
+public class TextFieldTest {
+    public static void main(String[] args) {
+        TextFieldFrame textFieldFrame = new TextFieldFrame();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(350, 100);
+        textFieldFrame.setVisible(true);
+    }
+} // end class TextFieldTest
